000fc6260c92fa420d4054934c7ea2d9224d5cc3
Antoine Méret <antoine.meret@gmail.com>
2025-09-24 18:29:36 +0200

    Prisma Postgres migration

--- prisma/migrations/20250701120449_add_manual_scoring/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "ComparisonEvaluation" ADD COLUMN "ingredientsScore" INTEGER;
-ALTER TABLE "ComparisonEvaluation" ADD COLUMN "instructionsScore" INTEGER;
-ALTER TABLE "ComparisonEvaluation" ADD COLUMN "titleScore" INTEGER;
-ALTER TABLE "ComparisonEvaluation" ADD COLUMN "totalScore" INTEGER;
-
--- CreateIndex
-CREATE INDEX "ComparisonEvaluation_totalScore_idx" ON "ComparisonEvaluation"("totalScore");
